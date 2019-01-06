fb95fd49608fa6362be9e0ca9928cb1793fd1e70
3A417029 <howardzhu0709@outlook.com>
2019-01-07 03:15:20 +0800

    UI 介面調整

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'NCUT Shop')</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
@@ -16,7 +16,7 @@
         </div>
         @include('layouts._footer')
     </div>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
     @yield('scriptsAfterJs')
 </body>
 </html> 
