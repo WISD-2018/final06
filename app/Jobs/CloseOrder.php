508afb2054d42f52e766a578a6a576a5b581c3b5
3A417029 <howardzhu0709@outlook.com>
2019-01-08 09:41:12 +0800

    用戶端:個別訂單

--- app/Jobs/CloseOrder.php
+++ app/Jobs/CloseOrder.php
@@ -9,19 +9,13 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\Models\Order;
 
-class CloseOrder.php implements ShouldQueue
+class CloseOrder implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
     protected $order;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function __construct(Order $order, $delay)
     {
-        //
         $this->order = $order;
         $this->delay($delay);
     }
@@ -38,7 +32,7 @@ class CloseOrder.php implements ShouldQueue
             return;
         }
         \DB::transaction(function() {
-            $this->order->update(['closed' => true]);
+            $this->order->update(['closed' => false]);
             foreach ($this->order->items as $item) {
                 $item->productSku->addStock($item->amount);
             }
--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -126,6 +126,7 @@
     axios.post('{{ route('orders.store') }}', req)
       .then(function (response) {
         alert('訂單送出成功', '', 'success');
+        location.href = '/orders/' + response.data.id;
       }, function (error) {
         if (error.response.status === 422) {
           var html = '<div>';
